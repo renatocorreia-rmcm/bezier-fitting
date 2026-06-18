9ddede0420476f899fdcd9cc14b97d8caea23339
rmcm <rmcm@g5c29.cin.ufpe.br>
2026-06-18 16:56:18 -0300

    fixed solution to underdetermined using pseudoinverse

--- bezier.py
+++ bezier.py
@@ -14,7 +14,7 @@ def fit(data_points, parameters, d):
     """
     fit a `d` degree Bézier curve to a sequence of Data Points
 
-    :return: control points, error, coefficients matrix
+    :return: control points, error
     """
 
     n = len(data_points)  # amount of data points
@@ -22,15 +22,17 @@ def fit(data_points, parameters, d):
     # setup coefficients matrix A
     A = get_coefficients_matrix(parameters, n, d)
 
-    if n == d:
-        # A * c = d or
+    if n == d:  # Determined: c = U⁻¹L⁻¹d
         control_points = np.linalg.solve(A, data_points)
         return control_points, np.linalg.norm((A@control_points)-data_points, axis=0)/n
 
-    elif n != d:
-        # (A^T) * A * c = (A^T) * d
-
+    elif n > d:  # OverDetermined: Solve c = (AᵀA)⁻¹Aᵀd
         control_points = np.linalg.solve(A.T @ A, A.T @ data_points)
         return control_points, np.linalg.norm((A@control_points)-data_points, axis=0)/n
+
+    elif n < d:  # UnderDetermined: Solve x = A⁺d
+        control_points = np.linalg.pinv(A)@data_points
+        return control_points, np.linalg.norm((A@control_points)-data_points, axis=0)/n
+
 
     return None
